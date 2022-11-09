11f8f92c0faa3144226b4c2e3fe50a72debca6fe
Sopiaa2 <https://github.com/Sopiaa2>
2022-11-09 17:55:27 +0900

    1차 수정

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+st.write('# 고백 app') 
+#1. 자신의 인적사항 기입
+i_love = []
+
+class loving_peolpe:
+    
+    def __init__(self, person):
+        self.person = person
+
+    
+    def some_function(self):
+        print(self.person)
+
+
+view = [100,200,300]
+view
+
+
+#2. 내가 좋아하는 사람 기입
+
+
+
+
+
+#3. 내가 좋아하는 사람 저장할 공간 (1명만 가능)
+
+#4. 또 다른 사람도 계속 기입
+
+#5. 계속 저장
+
+#6. 그러다가 서로 좋아하게 되면 매칭 or 아니면 꽝으로 얘기하거나 '짝사랑 중' 띄우기
+
+
+
